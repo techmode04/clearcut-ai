4f047c423e7151bce4c3f140d7c3352d2d648c4f
sachin <109057208+techmode04@users.noreply.github.com>
2025-12-31 12:40:33 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,20 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from rembg import remove
+from fastapi.responses import Response
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    output = remove(image_bytes)
+    return Response(content=output, media_type="image/png")
 
